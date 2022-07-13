e7413c1e89b6ea430992389a4765ba98413ccf42
Justen Doherty <justen.doherty@kurtgeiger.com>
2022-07-13 16:53:58 +0100

    added the aws cli

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+echo "add the aws cli"
+apk add --no-cache aws-cli
+
 echo "about to login to AWS ECR"
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_NUMBER.dkr.ecr.eu-west-1.amazonaws.com
 
