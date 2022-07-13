164554212ed1503c91226ed963409d95f51868e1
Justen Doherty <justen.doherty@kurtgeiger.com>
2022-07-13 14:46:30 +0100

    removed cleanup

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "removing old kg-node-14 docker container"
-docker stop kg-docker-nodejs-14 || true 
-docker rm kg-docker-nodejs-14 || true 
-docker system prune --all -f || true
+#echo "removing old kg-node-14 docker container"
+#docker stop kg-docker-nodejs-14 || true 
+#docker rm kg-docker-nodejs-14 || true 
+#docker system prune --all -f || true
 
-echo "cleaning up any kg-docker-nodejs-14 containers"
-docker rm -f $(docker ps -aq) || true 
+#echo "cleaning up any kg-docker-nodejs-14 containers"
+#docker rm -f $(docker ps -aq) || true 
 
-echo "building new kg-docker-nodejs-14 docker container"
+#echo "building new kg-docker-nodejs-14 docker container"
 docker build -t kg-docker-nodejs-14 .
