641814f6f5a7b2ffd4e36fbb700fdfdfa4982905
Justen Doherty <justen.doherty@kurtgeiger.com>
2022-07-15 11:22:55 +0100

    removed the aws bits

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "add the aws cli"
-apk add --no-cache aws-cli
+#echo "add the aws cli"
+#apk add --no-cache aws-cli
 
 #echo "about to login to AWS ECR"
 #aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_NUMBER.dkr.ecr.eu-west-1.amazonaws.com
